421abb5f40dee0bc249dca4abc71a45130bcf119
raghav <134798855+raghavnimbalkar1@users.noreply.github.com>
2025-02-12 07:47:24 +0530

    Delete server directory

--- server/models/Collection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const CollectionSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  name: { type: String, required: true },
-  type: { type: String, enum: ['Book', 'DVD', 'CD', 'Game'], required: true },
-  coverImage: { type: String },
-});
-module.exports = mongoose.model('Collection', CollectionSchema);
